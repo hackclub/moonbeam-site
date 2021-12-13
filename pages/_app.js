65e782066ce63355d6ff4a2991a253925200e526
eilla1 <git@ella.cx>
2021-12-12 18:28:32 -0800

    Add meta

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,27 @@ import NextApp from 'next/app'
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
+import Meta from '@hackclub/meta'
+import Head from 'next/head'
 import ColorSwitcher from '../components/color-switcher'
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <ThemeProvider theme={{
-        ...theme,
-        colors: { ...theme.colors, ...theme.colors.modes.dark, modes: {} }
-      }}>
+      <ThemeProvider
+        theme={{
+          ...theme,
+          colors: { ...theme.colors, ...theme.colors.modes.dark, modes: {} }
+        }}
+      >
+        <Meta
+          as={Head}
+          name="Hack Club"
+          title="Project Moonbeam"
+          description="We're literally sending Hack Club to the moon with codea and lasers."
+          color="#ec3750"
+        />
         <Component {...pageProps} />
       </ThemeProvider>
     )
